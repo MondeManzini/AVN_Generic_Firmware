78e37c15cde354bb8b87243d03e509ff4386b48a
root <root@MManzini.localdomain>
2021-09-13 13:58:06 +0200

    Added Version Ascii file.

---
+++ scr/Version_Ascii.vhd
@@ -0,0 +1,48 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+
+package Version_Ascii is
+
+constant A        : std_logic_vector(7 downto 0) := X"41";
+constant B        : std_logic_vector(7 downto 0) := X"42";
+constant C        : std_logic_vector(7 downto 0) := X"43";
+constant D        : std_logic_vector(7 downto 0) := X"44";
+constant E        : std_logic_vector(7 downto 0) := X"45";
+constant F        : std_logic_vector(7 downto 0) := X"46";
+constant G        : std_logic_vector(7 downto 0) := X"47";
+constant H        : std_logic_vector(7 downto 0) := X"48";
+constant I        : std_logic_vector(7 downto 0) := X"49";
+constant J        : std_logic_vector(7 downto 0) := X"4A";
+constant K        : std_logic_vector(7 downto 0) := X"4B";
+constant L        : std_logic_vector(7 downto 0) := X"4C";
+constant M        : std_logic_vector(7 downto 0) := X"4D";
+constant N        : std_logic_vector(7 downto 0) := X"4E";
+constant O        : std_logic_vector(7 downto 0) := X"4F";
+constant P        : std_logic_vector(7 downto 0) := X"50";
+constant Q        : std_logic_vector(7 downto 0) := X"51";
+constant R        : std_logic_vector(7 downto 0) := X"52";
+constant S        : std_logic_vector(7 downto 0) := X"53";
+constant T        : std_logic_vector(7 downto 0) := X"54";
+constant U        : std_logic_vector(7 downto 0) := X"55";
+constant V        : std_logic_vector(7 downto 0) := X"56";
+constant W        : std_logic_vector(7 downto 0) := X"57";
+constant X        : std_logic_vector(7 downto 0) := X"58";
+constant Y        : std_logic_vector(7 downto 0) := X"59";
+constant Z        : std_logic_vector(7 downto 0) := X"5A";
+constant Space    : std_logic_vector(7 downto 0) := X"20";
+constant Dot      : std_logic_vector(7 downto 0) := X"2E";
+
+constant Zero     : std_logic_vector(7 downto 0) := X"30";
+constant One      : std_logic_vector(7 downto 0) := X"31";
+constant Two      : std_logic_vector(7 downto 0) := X"32";
+constant Three    : std_logic_vector(7 downto 0) := X"33";
+constant Four     : std_logic_vector(7 downto 0) := X"34";
+constant Five     : std_logic_vector(7 downto 0) := X"35";
+constant Six      : std_logic_vector(7 downto 0) := X"36";
+constant Seven    : std_logic_vector(7 downto 0) := X"37";
+constant Eight    : std_logic_vector(7 downto 0) := X"38";
+constant Nine     : std_logic_vector(7 downto 0) := X"39";
+
+end Version_Ascii;
