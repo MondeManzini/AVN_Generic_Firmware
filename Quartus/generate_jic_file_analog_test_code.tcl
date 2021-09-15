8b4c3cfe90c3fb9079a038c7ff40d589d5bc24fa
root <root@MManzini.localdomain>
2021-09-15 13:01:32 +0200

    Updated jic file generating files.

--- Quartus/generate_jic_file_analog_test_code.tcl
+++ Quartus/generate_jic_file_analog_test_code.tcl
@@ -1,2 +1,2 @@
-exec quartus_cpf -c A1000_0137_FPGA_Controller_Test_Code.cof
-post_message "A1000_0137_FPGA_Controller_Test_Code jic Programming file generated"
+exec quartus_cpf -c Analog_Input_Test_Code.cof
+post_message "Analog_Input_Test_Code jic Programming file generated"
